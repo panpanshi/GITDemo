70404ed771d82e696f1c7b29a306cbfbd9eb2d2b
shipanpan <shipanpanyang@163.com>
2017-11-28 20:45:51 +0800

    Initial Commit

---
+++ GITDemo/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  GITDemo
+//
+//  Created by ShiPanpan on 2017/11/28.
+//  Copyright © 2017年 ShiPanpan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
